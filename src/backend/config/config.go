2d9b9a10b9d6b1774f0b9f45a67e80697f4e6fa0
Yuto Maeda <yuto.maeda@mendako.dev>
2026-09-20 01:05:28 +0900

    Add a config filed

--- src/backend/config/config.go
+++ src/backend/config/config.go
@@ -7,14 +7,17 @@ import (
 	"strconv"
 
 	"github.com/joho/godotenv"
+	"golang.org/x/oauth2"
+	"golang.org/x/oauth2/endpoints"
 )
 
 type Config struct {
 	OAuth2ClientId     string
 	OAuth2ClientSecret string
 	OAuth2RedirectUrl  string
+	OAuth2Config       *oauth2.Config
 	IsHttps            bool
-	JwtSecret          string
+	JwtSecret          []byte
 	DatabaseUrl        string
 }
 
@@ -29,13 +32,13 @@ func LoadConf() error {
 	clientId := os.Getenv("OAUTH2_CLIENT_ID")
 	clientSecret := os.Getenv("OAUTH2_CLIENT_SECRET")
 	redirectUrl := os.Getenv("OAUTH2_REDIRECT_URL")
-	isHttps, err := strconv.ParseBool(os.Getenv("IS_HTTPS"))
+	isHttps, parseBoolErr := strconv.ParseBool(os.Getenv("IS_HTTPS"))
 	jwtSecret := os.Getenv("JWT_SECRET")
 	dbUrl := os.Getenv("DB_URL")
 
 	if clientId == "" || clientSecret == "" || redirectUrl == "" {
 		return errors.New("OAuth2 environment variables are not set.")
-	} else if err != nil || jwtSecret == "" {
+	} else if parseBoolErr != nil || jwtSecret == "" {
 		return errors.New("Jwt environment variables are not set.")
 	} else if dbUrl == "" {
 		return errors.New("The database URL has not been set. Visit https://www.postgresql.org/docs/current/libpq-connect.html#LIBPQ-CONNSTRING-KEYWORD-VALUE to configure it.")
@@ -44,8 +47,15 @@ func LoadConf() error {
 	Conf.OAuth2ClientId = clientId
 	Conf.OAuth2ClientSecret = clientSecret
 	Conf.OAuth2RedirectUrl = redirectUrl
+	Conf.OAuth2Config = &oauth2.Config{
+		ClientID:     clientId,
+		ClientSecret: clientSecret,
+		RedirectURL:  redirectUrl,
+		Scopes:       []string{"identify"},
+		Endpoint:     endpoints.Discord,
+	}
 	Conf.IsHttps = isHttps
-	Conf.JwtSecret = jwtSecret
+	Conf.JwtSecret = []byte(jwtSecret)
 	Conf.DatabaseUrl = dbUrl
 
 	return nil
